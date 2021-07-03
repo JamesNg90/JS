7b89749d3ff3fae6b05a7eca1de2a864474fe3dd
JamesNg90 <86848706+JamesNg90@users.noreply.github.com>
2021-07-03 21:58:36 +1000

    Add files via upload

---
+++ Everything.js
@@ -0,0 +1,22 @@
+// 1st version using a loop
+function everyloop(array,test){
+    let result = true;
+    for (let i of array){
+        result = result && test(i) 
+    }
+    return result;
+}
+
+console.log(everyloop([1, 3, 5], n => n < 10));
+console.log(everyloop([2, 4, 16], n => n < 10));
+console.log(everyloop([], n => n < 10));
+
+// 2nd version using some method
+function everysome(array,test){
+    result = !array.some(e => !test(e))
+    return result;    
+}
+
+console.log(everysome([1, 3, 5], n => n < 10));
+console.log(everysome([2, 4, 16], n => n < 10));
+console.log(everysome([], n => n < 10));
