5bd97a811819418e7c540781592fd02e9ce786ae
JamesNg90 <86848706+JamesNg90@users.noreply.github.com>
2021-07-04 02:24:54 +1000

    Update FizzBuzz.js

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,4 +1,7 @@
-// uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead. print "FizzBuzz" for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+// uses console.log to print all the numbers from 1 to 100, with two exceptions. 
+//For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), 
+//print "Buzz" instead. print "FizzBuzz" for numbers that are divisible by both 3 and 5 
+//(and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
 
 for (let num = 1; num <=100; num++) {
     if (num % 3 == 0 && num % 5 != 0) {
